4708ffa4a8505d33437a7ce10991f396cff59691
Young-Il Seo <yiseo1011@gmail.com>
2019-07-20 23:10:48 +0900

    #8 Hough Transform 2

--- lanes.py
+++ lanes.py
@@ -1,3 +1,5 @@
+# https://youtu.be/eLTLtUVuuy4
+
 import cv2
 import numpy as np
 # import matplotlib.pyplot as plt
@@ -31,6 +33,15 @@ def canny(image):
 
     return canny
 
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            # print(line) [[x1, y1, x2, y2]]
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1,y1), (x2,y2), (255, 0, 0), 10)
+
+    return line_image
 
 '''
 #5 Region of Interest
@@ -60,14 +71,20 @@ image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 canny = canny(lane_image)
 cropped_image = region_of_interest(canny)
-#plt.imshow(canny)
-#plt.show()
-
 '''
 #7 Hough Transform
     finding possible line by hough space 
     can determine ax + b
-'''
 
-cv2.imshow("result", cropped_image)
+#8 Hough Transform 2
+'''
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100,
+                        np.array([]), minLineLength=40, maxLineGap=5)
+line_image  = display_lines(lane_image, lines)
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+
+#plt.imshow(canny)
+#plt.show()
+
+cv2.imshow("result", combo_image)
 cv2.waitKey(0)
