d4365a4b2e5c124621a0e87197d320e38d4f57e6
Young-Il Seo <yiseo1011@gmail.com>
2019-07-20 23:25:35 +0900

    #9 Optimization

--- lanes.py
+++ lanes.py
@@ -4,6 +4,40 @@ import cv2
 import numpy as np
 # import matplotlib.pyplot as plt
 
+'''
+#9 Optimization
+'''
+
+def make_coordinates(image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1 * (3 / 5))
+    x1 = int((y1 - intercept) / slope)
+    x2 = int((y2 - intercept) / slope)
+
+    return np.array([x1, y1, x2, y2])
+
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0 :
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+
+    left_line = make_coordinates(image, left_fit_average)
+    right_line = make_coordinates(image, right_fit_average)
+
+    return np.array([left_line, right_line])
+
 def canny(image):
     '''
     #2 : Grayscale Conversion
@@ -16,7 +50,7 @@ def canny(image):
             3 channels to 1 channel(one intensity)
             = fast processing
     '''
-    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
     '''
     #3 : Gaussian Blur
@@ -36,9 +70,8 @@ def canny(image):
 def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
-        for line in lines:
+        for x1, y1, x2, y2 in lines:
             # print(line) [[x1, y1, x2, y2]]
-            x1, y1, x2, y2 = line.reshape(4)
             cv2.line(line_image, (x1,y1), (x2,y2), (255, 0, 0), 10)
 
     return line_image
@@ -69,8 +102,8 @@ def region_of_interest(image):
 
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
-canny = canny(lane_image)
-cropped_image = region_of_interest(canny)
+canny_image = canny(lane_image)
+cropped_image = region_of_interest(canny_image)
 '''
 #7 Hough Transform
     finding possible line by hough space 
@@ -80,7 +113,8 @@ cropped_image = region_of_interest(canny)
 '''
 lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100,
                         np.array([]), minLineLength=40, maxLineGap=5)
-line_image  = display_lines(lane_image, lines)
+averaged_lines = average_slope_intercept(lane_image, lines)
+line_image  = display_lines(lane_image, averaged_lines)
 combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
 
 #plt.imshow(canny)
