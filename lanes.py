c2aa9b512571e3386bc7d21716fc26ae42250a08
Young-Il Seo <yiseo1011@gmail.com>
2019-07-20 22:04:01 +0900

    #2 Grayscale Conversion

--- lanes.py
+++ lanes.py
@@ -1,7 +1,31 @@
 import cv2
+import numpy as np
 
-''' imread, imshow '''
+'''
+#1 : Installation
+imread, imshow
+'''
+
 image = cv2.imread('test_image.jpg')
+lane_image = np.copy(image)
 
-cv2.imshow('result', image)
+'''
+#2 : Findding Lane Lines(Grayscale Conversion)
+Edge Detection : identifying sharp changes in intensity in adjacent pixels
+Gradient : Measure of change in brightness over adjacent pixels
+
+edge : rapid changes in brightness
+
+step 1 : gray scale
+    3 channels to 1 channel(one intensity)
+    = fast processing
+'''
+gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+
+'''
+#3 : Finding Lane Lines(Gaussian Blur)
+
+'''
+cv2.imshow('result', gray)
 cv2.waitKey(0)
+
